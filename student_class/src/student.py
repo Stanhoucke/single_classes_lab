1f311637d57a3df6a5cc5614575e48796a106047
Stanley Houcke <stanhoucke@gmail.com>
2020-11-23 14:55:37 +0000

    passed all tests

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -1,6 +1,17 @@
 # Create a class called Student
 class Student:
     
+    # Initialise student
     def __init__(self, input_name, input_cohort):
         self.name = input_name
         self.cohort = input_cohort
+
+
+    # Define talk function
+    def talk(self):
+        return "I can talk!"
+
+
+    # Define favourite languate
+    def say_favourite_language(self, language):
+        return "I love " + language
