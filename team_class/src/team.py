96c154b713c2c829e9b9b3be3316ebdc293ff97a
Stanley Houcke <stanhoucke@gmail.com>
2020-11-23 15:11:09 +0000

    passed test 1 in Team

---
+++ team_class/src/team.py
@@ -0,0 +1,10 @@
+# Create a class called Team
+
+class Team:
+
+    def __init__(self, input_team_name, input_players, input_coach):
+        self.name = input_team_name
+        self.players = input_players
+        self.coach = input_coach
+
+        
