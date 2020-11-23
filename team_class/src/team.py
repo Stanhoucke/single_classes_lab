aed54f61cad7fe8a32adc8dc35bc7810238d0821
Stanley Houcke <stanhoucke@gmail.com>
2020-11-23 15:50:59 +0000

    passed test 6 in Team

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -1,5 +1,4 @@
 # Create a class called Team
-
 class Team:
 
     def __init__(self, input_team_name, input_players, input_coach):
@@ -11,4 +10,12 @@ class Team:
     # Define function to add new player
     def add_player(self, new_player):
         self.players.append(new_player)
-        
+
+
+    # Define function to search for player in team
+    def has_player(self, checked_player):
+        for player in self.players:
+            if player == checked_player:
+                return True
+        # if player is not in team
+        return False
